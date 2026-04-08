872a430928869fecf9cd3e7ea8a3df1f5068259b
Justin Lin <wancat@wancat.cc>
2026-04-08 20:46:48 +1000

    feat: set english and chinese fonts (by Google Fonts)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './layouts/**/*.html',
-    './content/**/*.{html,md}',
-  ],
+  content: ["./layouts/**/*.html", "./content/**/*.{html,md}"],
   theme: {
     extend: {
       colors: {
-        'light-gray': '#f5f5f5',
+        "light-gray": "#f5f5f5",
       },
       fontFamily: {
-        sans: ['-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'sans-serif'],
+        sans: ['"Funnel Sans"', "ui-sans-serif", "system-ui", "sans-serif"],
+        tc: [
+          '"Noto Sans TC"',
+          '"Funnel Sans"',
+          "ui-sans-serif",
+          "system-ui",
+          "sans-serif",
+        ],
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
