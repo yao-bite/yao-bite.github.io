a691fb8fff0d641843b912d5dbf88dab9010e3ee
Justin Lin <wancat@wancat.cc>
2026-04-08 21:44:51 +1000

    feat: use rxjs to implement album prev/next

---
+++ static/js/album.js
@@ -0,0 +1,35 @@
+const { range, filter, map, merge, fromEvent, scan } = rxjs;
+
+function registerAlbum(id, total) {
+  range(1, 10).subscribe((x) => console.log(x, id));
+
+  var current = 0;
+
+  var strip = document.getElementById(id + "-strip");
+  var prevBtn = document.getElementById(id + "-prev");
+  var nextBtn = document.getElementById(id + "-next");
+
+  function update(current) {
+    strip.style.transform = "translateX(-" + current * 100 + "%)";
+    prevBtn.disabled = current === 0;
+    nextBtn.disabled = current === total - 1;
+    for (var i = 0; i < total; i++) {
+      var dot = document.getElementById(id + "-dot-" + i);
+      if (dot)
+        dot.className =
+          "w-1.5 h-1.5 rounded-full transition-colors " +
+          (i === current ? "bg-gray-800" : "bg-gray-300");
+    }
+  }
+
+  prev = fromEvent(prevBtn, "click").pipe(
+    map(() => (c) => (c > 0 ? c - 1 : c)),
+  );
+  next = fromEvent(nextBtn, "click").pipe(
+    map(() => (c) => (c < total - 1 ? c + 1 : c)),
+  );
+
+  merge(prev, next)
+    .pipe(scan((current, reducer) => reducer(current), 0))
+    .subscribe(update);
+}
